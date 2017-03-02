e61268bee98a41423e40099c896e8a9d5016e8f7
Kyle Anderson <kwa@yelp.com>
2017-03-01 18:09:09 -0800

    wip

--- paasta_tools/docker_wrapper.py
+++ paasta_tools/docker_wrapper.py
@@ -22,6 +22,7 @@ import re
 import socket
 import sys
 
+import binpacking
 
 ENV_MATCH_RE = re.compile('^(-\w*e\w*|--env)(=(\S.*))?$')
 MAX_HOSTNAME_LENGTH = 63
@@ -79,21 +80,18 @@ def generate_hostname(fqdn, mesos_task_id):
 def add_argument(args, argument):
     # Add an argument immediately after 'run' command if it exists
     args = list(args)
-
     try:
         run_index = args.index('run')
     except ValueError:
         pass
     else:
         args.insert(run_index + 1, argument)
-
     return args
 
 
 def get_core_list(cpuid):
     core = 0
     core_list = []
-
     try:
         with open('/proc/cpuinfo', 'r') as f:
             for line in f:
@@ -104,12 +102,90 @@ def get_core_list(cpuid):
                     core += 1
     except IOError:
         pass
-
     return core_list
+
+
+def prune_dead_pids(bins):
+    # TODO: check pids
+    return bins
+
+
+def get_existing_bins():
+    if True:  # file exists
+        # TODO: Serialize
+        existing_bins = [
+            {101: 6, 102: 2.0}, {}
+        ]
+        existing_bins = prune_dead_pids(existing_bins)
+        return existing_bins
+    else:
+        print("First time running, new bins")
+        return []
+
+
+def binpack(bins, value, max_bin_capacity, max_bins):
+    # TODO: binpack for realz
+    incoming_key = value[0]
+    incoming_weight = value[1]
+    print("Trying to pack %s:%s into the current bins: %s" % (incoming_key, incoming_weight, bins))
+    print("with a max bin capacity of %s and not using more than %s bins" % (max_bin_capacity, max_bins))
+    for bin in bins:
+        bin_capacity = sum(bin.values())
+        if bin_capacity + incoming_weight > max_bin_capacity:
+            continue
+        else:
+            bin[incoming_key] = incoming_weight
+            return bins
+    # If we got here, maybe we can try starting a new bin
+    if len(bins) < max_bins:
+        return max_bins.append({incoming_key[incoming_weight]})
+    else:
+        raise ValueError("Couldn't find a place for %s:%s" % (incoming_key[incoming_weight]))
 
 
 def is_numa_enabled():
     return os.path.exists('/proc/1/numa_maps')
+
+
+def get_requested_cpu(env_args):
+    # TODO: do better
+    return float(env_args.get('MARATHON_APP_RESOURCE_CPUS', '10'))
+
+
+def pick_best_numa_zone(requested_cpu):
+    numa_zones = 2
+    cores_per_zone = len(get_core_list(0))
+    pid = os.getpid()
+    existing_bins = get_existing_bins()
+
+    try:
+        new_bins = binpack(
+            bins=existing_bins,
+            value=(pid, requested_cpu),
+            max_bin_capacity=cores_per_zone,
+            max_bins=numa_zones,
+        )
+    except ValueError:
+        return None
+
+    print("After packing, here is what the new bins look like: %s" % new_bins)
+
+    for bin in new_bins:
+        if bin.get(pid, False):
+            print("Guessing the best place to put this is on numa zone %d" % new_bins.index(bin))
+            return new_bins.index(bin)
+
+    print("Couldn't find a place to bin. Shouldn't get here!")
+    return None
+
+
+def get_numa_args(requested_cpu):
+    numa_zone = pick_best_numa_zone(requested_cpu)
+    if numa_zone == None:
+        return ""
+    else:
+        core_list = get_core_list(numa_zone)
+        return '--cpuset-cpus=%s --cpuset-mems=%s' % (','.join(str(c) for c in core_list), str(numa_zone))
 
 
 def main(argv=None):
@@ -121,19 +197,18 @@ def main(argv=None):
     mesos_task_id = env_args.get('MESOS_TASK_ID') or env_args.get('mesos_task_id')
 
     # Invalid the variable if it has a bogus value
-    try:
-        numa_cpuid = int(env_args.get('PIN_TO_NUMA_NODE'))
-    except (ValueError, TypeError):
-        numa_cpuid = None
+    pin_to_numa = bool(env_args.get('PIN_TO_NUMA_NODE', False))
+    pin_to_numa = True
 
-    if numa_cpuid and is_numa_enabled():
-        core_list = get_core_list(numa_cpuid)
-        if len(core_list) > 0:
-            argv = add_argument(argv, '--cpuset-cpus=' + ','.join(str(c) for c in core_list))
-            argv = add_argument(argv, '--cpuset-mems=' + str(numa_cpuid))
+    if pin_to_numa:
+        if is_numa_enabled():
+            requested_cpu = get_requested_cpu(env_args)
+            argv = add_argument(argv, get_numa_args(requested_cpu=requested_cpu))
+        else:
+            print("Warning: asked for NUMA pinning but not on a NUMA-enabled machine!")
 
     if mesos_task_id and not already_has_hostname(argv):
         hostname = generate_hostname(socket.getfqdn(), mesos_task_id)
         argv = add_argument(argv, '--hostname=' + hostname)
 
-    os.execlp('docker', 'docker', *argv[1:])
+    print(argv)
